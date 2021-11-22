65319a119a84604d4844c63c78bcf802ceeb4266
Kaitaku Takeda <katakeda32@gmail.com>
2021-11-22 00:22:48 -0800

    Add dotenv

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/katakeda/boardhop-api-service-go/app"
 )
 
 func main() {
+	godotenv.Load()
+
 	app := app.App{}
 	app.Initialize()
 	app.Run()
